f04680b61ae8a23e5331d89414a2ea2c837fe209
GitHub Action <action@github.com>
2024-04-22 21:08:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/tbsten/aspluginpractice/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/tbsten/aspluginpractice/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tbsten.aspluginpractice.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/tbsten/aspluginpractice/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tbsten/aspluginpractice/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.tbsten.aspluginpractice.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tbsten.aspluginpractice.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
